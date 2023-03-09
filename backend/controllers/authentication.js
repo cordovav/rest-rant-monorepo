c877cae207d5cff4f4cace3d94d4a7c62639d46e
Veny Cordova <venycordova@gmail.com>
2023-03-08 21:16:34 -0500

    using json web token authentication

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const db = require("../models")
 const bcrypt = require('bcrypt')
+const jwt = require('json-web-token')
 
 const { User } = db
 
@@ -15,8 +16,34 @@ router.post('/', async (req, res) => {
             message: `Could not find a user with the provided username and password`
         })
     } else {
-        res.json({ user })
+        const result = await jwt.encode(process.env.JWT_SECRET, { id: user.userId })
+        res.json({ user: user, token: result.value })
     }
 })
 
+router.get('/profile', async (req,res) =>{
+    try {
+        //split the authorization header into bearer and token
+        const [authenticationMethod, token] = req.headers.authorization.split( ' ' )
+
+        //only handle bearer authorization for now
+        //we can add other authorization later
+        if (authenticationMethod == 'Bearer') {
+            //decode the jwt
+            const result = await jwt.decode(process.env.JWT_SECRET, token)
+            //get the logged in users id from the payload
+            const { id } = result.value
+            
+            //find the user object using there id
+            let user = await User.findOne({
+                where: {
+                    userId: id
+                }
+            })
+            res.json(user)
+        }
+    }catch {
+        res.json(null)
+    }
+})
 module.exports = router
